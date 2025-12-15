b62b2309cb6913762a4a0b30c2cdc5d9c6167ec0
GBlecher <gilanblecher@gmail.com>
2025-12-15 08:25:21 +0200

    upload

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,41 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import InitialLayout from "@/components/InitialLayout";
-
 import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
-// import { SplashScreen } from "expo-router";
-// import { useFonts } from "expo-font";
-// import { useCallback, useEffect } from "react";
-// import * as NavigationBar from "expo-navigation-bar";
-// import { Platform } from "react-native";
+import { SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import { useCallback, useEffect } from "react";
+import * as NavigationBar from "expo-navigation-bar";
+import { Platform } from "react-native";
 
-// import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar";
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-
-if (!publishableKey) {
-  throw new Error("Clerk publishable key is not defined set in .env");
-}
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  // update the native navigation bar on Android.
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      NavigationBar.setBackgroundColorAsync("#000000");
+      NavigationBar.setButtonStyleAsync("light");
+    }
+  }, []);
+
   return (
     <ClerkAndConvexProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }} onLayout={onLayoutRootView}>
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
+      <StatusBar style="light" />
     </ClerkAndConvexProvider>
   );
 }
---
+++ components/Post.tsx
@@ -0,0 +1,140 @@
+import { COLORS } from "@/constants/theme";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { styles } from "@/styles/feed.styles";
+import { Ionicons } from "@expo/vector-icons";
+import { useMutation } from "convex/react";
+// import { useMutation, useQuery } from "convex/react";
+import { Image } from "expo-image";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+// import CommentsModal from "./CommentsModal";
+// import { formatDistanceToNow } from "date-fns";
+// import { useUser } from "@clerk/clerk-expo";
+
+type PostProps = {
+  
+  post: {
+    _id: Id<"posts">;
+    imageUrl: string;
+    caption?: string;
+    likes: number;
+    comments: number;
+    _creationTime: number;
+    isLiked: boolean;
+    isBookmarked: boolean;
+    author: {
+      _id: string;
+      username: string;
+      image: string;
+    };
+  };
+};
+
+export default function Post({ post }: PostProps) {
+ 
+ const [isLiked, setIsLiked] = useState(post.isLiked);
+ const [likescount, setLikesCount] = useState(post.likes);
+ 
+ const toggleLike = useMutation(api.posts.toggleLike);
+ 
+ const handleLike = async () => {
+    try {
+      const newIsLiked = await toggleLike({postId: post._id});
+      setIsLiked(newIsLiked);
+      setLikesCount((prev) => (newIsLiked ? prev + 1 : prev - 1));
+    } catch (error) {
+      console.error("Error toggling like:", error);
+    }
+ }
+ 
+ 
+ 
+ return (
+
+    <View>
+      {/* POST HEADER */}
+
+      <View style={styles.postHeader}>
+        <Link href={"/(tabs)/notifications"}>
+          <TouchableOpacity style={styles.postHeaderLeft}>
+            <Image
+              source={post.author.image}
+              style={styles.postAvatar}
+              contentFit="cover"
+              transition={200}
+              cachePolicy="memory-disk"
+            />
+            <Text style={styles.postUsername}>{post.author.username}</Text>
+          </TouchableOpacity>
+        </Link>
+
+        {/* if i'm the owner of the post, show the delete button 
+        {post.author._id === currentUser?._id ? (
+          <TouchableOpacity onPress={handleDelete}>
+            <Ionicons name="trash-outline" size={20} color={COLORS.primary} />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity>
+            <Ionicons
+              name="ellipsis-horizontal"
+              size={20}
+              color={COLORS.white}
+            />
+          </TouchableOpacity>
+        )} */}
+
+        <TouchableOpacity>
+            <Ionicons name= "trash-outline" size={20} color={COLORS.primary} />
+        </TouchableOpacity>
+      </View>
+
+      {/* IMAGE */}
+      <Image
+        source={post.imageUrl}
+        style={styles.postImage}
+        contentFit="cover"
+        transition={200}
+        cachePolicy="memory-disk"
+      />
+
+      {/* POST ACTIONS */}
+
+      <View style={styles.postActions}>
+        <View style={styles.postActionsLeft}>
+          <TouchableOpacity onPress={handleLike}>
+            <Ionicons name={isLiked ? "heart" : "heart-outline"} size={24} color={isLiked ? COLORS.primary : COLORS.white} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name="chatbubble-outline" size={22} color={COLORS.white} />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity>
+          <Ionicons name="bookmark-outline" size={22} color={COLORS.white} />
+        </TouchableOpacity>
+      </View>
+
+       {/* POST INFO */}
+      <View style={styles.postInfo}>
+       <Text style={styles.likesText}>
+        {likescount > 0 ? `${likescount.toLocaleString()} likes` : "Be the first to like"}
+       </Text>
+       {post.caption && (
+        <View style={styles.captionContainer}>
+          <Text style={styles.captionUsername}>{post.author.username}</Text>
+          <Text style={styles.captionText}>{post.caption}</Text>
+        </View>
+       )}
+
+       <TouchableOpacity>
+        <Text style={styles.commentsText}>View all 2 comments</Text>
+       </TouchableOpacity>
+
+       <Text style={styles.timeAgo}>2 hours ago</Text>
+      </View>
+        
+  
+    </View>
+  );
+}
